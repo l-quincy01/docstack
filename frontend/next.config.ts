3c4e2b0ce7c2f039873477a82afcc162d695bd09
Quincy Pitsi <81038011+l-quincy01@users.noreply.github.com>
2026-03-28 00:02:55 +0200

    Revert "Feat/math plugin"

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  turbopack: {},
-  onDemandEntries: {
-    maxInactiveAge: 15 * 1000,
-    pagesBufferLength: 1,
-  },
+  /* config options here */
 };
 
 export default nextConfig;
